83b2fad4572e7aefb78d648c091c8d7d63c0cc6f
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-04 08:34:56 +0530

    argparse added

--- base/__main__.py
+++ base/__main__.py
@@ -2,20 +2,24 @@
 
 
 import sys
+import argparse
+
 from .classmodule import MyClass
 from .funcmodule import my_function
 
 def main():
     print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
 
-    my_function('hello world')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, default='google',
+		        help='Name of the stock for price') #P change this to something meaningful   
+    args =  parser.parse_args()
+    print('Parser added')
 
-    my_object = MyClass('Debjyoti')
-    my_object.say_name()
+    #my_function('hello world')
+
+    #my_object = MyClass('Debjyoti')
+    #my_object.say_name()
 
 if __name__ == '__main__':
     main()
