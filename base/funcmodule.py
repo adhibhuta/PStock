f3a3cfdacfdcd69ffd532bb6329efc5d3d6df473
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-04 08:39:05 +0530

    changed funcmodule -- D think of meaningful name

--- base/funcmodule.py
+++ base/funcmodule.py
@@ -1,6 +1,7 @@
 #Author: lladhibhutall 
 
 
-def my_function(text_to_display):
-    print('text from my_function :: {}'.format(text_to_display))
+def my_function(args):
+	name = args.name
+	return name 
 
