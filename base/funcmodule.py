477fe045ac6f3d8fd49be05957fa834892ef0391
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-05 01:19:35 +0530

    API key added and returning data for GLOBAL_QUOTE

--- base/funcmodule.py
+++ base/funcmodule.py
@@ -1,8 +1,10 @@
 #Author: lladhibhutall 
-
+import requests
 
 def my_function(args):
 	symbol = args.symbol
-	link =  "https://www.alphavantage.co/query?function=GLOBAL_QUOTES&symbol={}&apikey=demo".format(symbol)
-	return link 
+	link =  "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey=O45RHWNY6BI3SSGW".format(symbol)
+	r = requests.get(link)
+	json_data = r.json()
+	return str(json_data) 
 
