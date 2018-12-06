a2fe49f92ef4ca939448f562080ae27bd64ede82
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-07 00:04:53 +0530

    cleaned up the file

--- base/__main__.py
+++ base/__main__.py
@@ -1,8 +1,6 @@
 #author: lladhibhutall 
 
 
-import sys
-import argparse
 import click
 
 from .classmodule import MyClass
@@ -11,16 +9,9 @@ from .funcmodule import my_function
 @click.command()
 @click.argument('symbol')
 def main(symbol):
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument('--symbol', type=str, default='googl',
-		        #help='Name of the stock for price') #P change this to something meaningful   
-    #args =  parser.parse_args()
+    
     click.echo(my_function(symbol))
 
-    #my_function('hello world')
-
-    #my_object = MyClass('Debjyoti')
-    #my_object.say_name()
 
 if __name__ == '__main__':
     main()
