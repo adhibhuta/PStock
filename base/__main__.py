162d8a2cb5f49f3f853426f4c69f9af437ad630e
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-07 00:00:01 +0530

    made first click changes to the file

--- base/__main__.py
+++ base/__main__.py
@@ -9,12 +9,13 @@ from .classmodule import MyClass
 from .funcmodule import my_function
 
 @click.command()
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--symbol', type=str, default='googl',
-		        help='Name of the stock for price') #P change this to something meaningful   
-    args =  parser.parse_args()
-    sys.stdout.write(my_function(args))
+@click.argument('symbol')
+def main(symbol):
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('--symbol', type=str, default='googl',
+		        #help='Name of the stock for price') #P change this to something meaningful   
+    #args =  parser.parse_args()
+    click.echo(my_function(symbol))
 
     #my_function('hello world')
 
