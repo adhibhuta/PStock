6bcc1edcd40ab5e50694c74e911e5a7775cf6ef0
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-06 23:36:09 +0530

    added requirements click and requests

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name = 'PStock',
     version = '0.0.1',
     packages = ['base'],
+    install_requires=['click','requests'],
     entry_points = {
         'console_scripts': [
             'PStock = base.__main__:main'
