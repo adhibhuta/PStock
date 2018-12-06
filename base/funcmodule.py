898eb8243c91eac83ff732d1f125ec5e22257cf0
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-07 00:01:01 +0530

    changes made to work with click

--- base/funcmodule.py
+++ base/funcmodule.py
@@ -2,8 +2,8 @@
 import requests
 
 def my_function(args):
-	symbol = args.symbol
-	link =  "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey=O45RHWNY6BI3SSGW".format(symbol)
+	#symbol = args.symbol
+	link =  "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey=O45RHWNY6BI3SSGW".format(args)
 	r = requests.get(link)
 	json_data = r.json()
 	return str(json_data) 
