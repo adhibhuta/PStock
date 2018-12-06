cc55a95cd860e7b6dcf3d6067222ea044d78fb76
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-07 00:31:33 +0530

    Added docstrings for argument description

--- base/__main__.py
+++ base/__main__.py
@@ -9,7 +9,7 @@ from .funcmodule import my_function
 @click.command()
 @click.argument('symbol')
 def main(symbol):
-    
+    ''' Enter the ticker symbol ''' #P look into this and give a meaningful description    
     click.echo(my_function(symbol))
 
 
