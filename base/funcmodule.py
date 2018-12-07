6b5c31e992421f250733f8140343c56500fa2934
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-08 00:39:49 +0530

    Intraday data

--- base/funcmodule.py
+++ base/funcmodule.py
@@ -3,7 +3,7 @@ import requests
 
 def my_function(args):
 
-	link =  "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey=O45RHWNY6BI3SSGW".format(args)
+	link =  "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval=5min&outputsize=full&apikey=O45RHWNY6BI3SSGW".format(args)
 	r = requests.get(link)
 	json_data = r.json()
 	return str(json_data) 
