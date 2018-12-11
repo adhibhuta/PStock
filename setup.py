0608ffce23afdb04ed78f862d555777b4805ca13
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-11 18:36:23 +0530

    plotile added to setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'PStock',
     version = '0.0.1',
     packages = ['base'],
-    install_requires=['click','requests','json','drawille'],
+    install_requires=['click','requests','json','plotille'],
     entry_points = {
         'console_scripts': [
             'PStock = base.__main__:main'
