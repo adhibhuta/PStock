3ae0dd9f62daa5fb9dc36127a96b2156ad19a6ce
lladhibhutall <debjyoti.biswas@outlook.com>
2018-12-14 22:14:56 +0530

    Added more details to setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,18 @@ setup(
     name = 'PStock',
     version = '0.0.1',
     packages = ['base'],
+    description = 'Commandline utility to check stock details',
+    author = 'lladhibhutall',
+    author_email = 'debjyoti.biswas@outlook.com',
+    url = 'https://github.com/lladhibhutall',
     install_requires=['click','requests','plotille'],
+    license='Apache2',
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3'
+      ],
     entry_points = {
         'console_scripts': [
             'PStock = base.__main__:main'
